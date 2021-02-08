50bb766e507f76546bc4cc0913ea726b11c83647
xxkun <332271571@qq.com>
2021-02-08 15:16:24 +0800

    project init

---
+++ src/main/java/com/xxkun/ppepository/PpepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.xxkun.ppepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpepositoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xxkun/ppepository/PpepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxkun.ppepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
