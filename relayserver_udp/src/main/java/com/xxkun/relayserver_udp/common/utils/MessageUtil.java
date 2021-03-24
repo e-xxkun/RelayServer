3dc8a5e1cdd5879831bd8dba5e177ba230d22726
xxkun <332271571@qq.com>
2021-03-24 22:58:49 +0800

    fix UDPField

--- relayserver_udp/src/main/java/com/xxkun/relayserver_udp/common/utils/MessageUtil.java
+++ relayserver_udp/src/main/java/com/xxkun/relayserver_udp/common/utils/MessageUtil.java
@@ -1,4 +1,11 @@
 package com.xxkun.relayserver_udp.common.utils;
 
+import com.xxkun.relayserver_udp.dao.UDPField;
+
+import java.net.DatagramPacket;
+
 public class MessageUtil {
+    public static DatagramPacket decodeUDPField(UDPField udpField) {
+        return null;
+    }
 }
