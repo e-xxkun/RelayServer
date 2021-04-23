94c26eb91c7712cbecbb196b2ef4c7b4118c90fe
e-xxkun <e_xxkun@163.com>
2021-04-23 23:01:18 +0800

    rebuilding

--- relayserver_udp/src/main/java/com/xxkun/relayserver/pojo/response/UpdateTokenResponse.java
+++ relayserver_udp/src/main/java/com/xxkun/relayserver/pojo/response/UpdateTokenResponse.java
@@ -1,7 +1,9 @@
 package com.xxkun.relayserver.pojo.response;
 
+import com.xxkun.relayserver.common.TokenProducer;
 import com.xxkun.relayserver.component.exception.ResponseConvertException;
 import com.xxkun.relayserver.pojo.ResponseType;
+import com.xxkun.relayserver.pojo.request.Request;
 import com.xxkun.relayserver.pojo.user.UserInfo;
 import com.xxkun.udptransfer.TransferPacket;
 
@@ -24,7 +26,7 @@ public class UpdateTokenResponse extends Response{
 
     @Override
     public int getBodyLength() {
-        return userInfo.bytesLength();
+        return userInfo.getSession().getToken().length() * Character.BYTES;
     }
 
     @Override
